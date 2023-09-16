92c7398e67961cd55776f5f7b20dae67782bb8b0
Atharv Vatsal <atharvvatsal@outlook.com>
2023-09-16 20:46:14 +0530

    2 Python Files Added

---
+++ input output.py
@@ -0,0 +1,9 @@
+name = input("Enter your name ")
+birth = int(input("Enter your Birth Year "))
+age = 2023 - birth
+sex = input("Gender ")
+print(f""" Biodata>>>
+      Name: {name}
+      DOB: {birth}
+      Age: {age}
+      Gender: {sex}""")
