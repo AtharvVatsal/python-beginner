bb9ddce0edc735bc1f174e27e2a9f8b8121808a3
Atharv Vatsal <atharvvatsal@outlook.com>
2023-09-17 15:20:41 +0530

    Testing Creation of a new Parallel Branch

--- hello world.py
+++ hello world.py
@@ -1,1 +1,12 @@
-print("Hello World")
+name = input("Enter Your name -> ")
+gender = input("Whats Your Gender (M) or (F) ->").upper()
+if len(name) <= 6:
+    print("A SHORT NAME")
+else:
+    print("A BIG NAME")
+if gender == 'M':
+    print(f"Hello Mr. {name}")
+elif gender == 'F':
+    print(f"Hello Ms/Mrs. {name} ")
+else:
+    print("There are only Two Genders")
