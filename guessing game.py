51a4806bf3016782d28c3c6eaf0ee33a37f58655
Atharv Vatsal <atharvvatsal@outlook.com>
2023-09-17 10:54:40 +0530

    Added New While loop files and guessing game

---
+++ guessing game.py
@@ -0,0 +1,11 @@
+secret_no = 5
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit :
+    user_guess = int(input('Guess: '))
+    guess_count += 1
+    if (user_guess == secret_no):
+        print("YOU WON!")
+        break
+else:
+    print("Lost It, Better Luck Next Time")
---
+++ while loops.py
@@ -0,0 +1,6 @@
+i = 1
+while i <= 5:       #while (condition):
+    print('%' * i)
+    i = i + 1
+    print('@' * i)
+print('Exectution Done')
