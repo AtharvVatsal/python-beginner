27bb4f184652cafe23da8743f90f240315450085
Atharv Vatsal <atharvvatsal@outlook.com>
2023-09-19 16:11:38 +0530

    Lists Added

--- lists.py
+++ lists.py
@@ -1,3 +1,15 @@
-str = 'qweas'
-a = str.index('e')
-print(a)
+l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+max = l[0]
+for values in l:
+    if values > max:
+        max = values
+print(max)
+
+#Randomised list
+
+random = [33, 546, 232, 85412, 946, 585412]
+num = random[0]
+for count in random:
+    if count > num:
+        num = count
+print(num)
